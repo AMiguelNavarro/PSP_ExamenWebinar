fc101d6c76ad18d156d71813ba08adb2eaf75f0f
Mike <alberto.miguel96@gmail.com>
2021-01-22 21:24:45 +0100

    Optimización código

--- src/main/java/Task/AppTask.java
+++ src/main/java/Task/AppTask.java
@@ -32,83 +32,110 @@ public class AppTask extends Task<Void> {
 
         if (!iniciarCuentaAtras) {
 
-            int porcentaje;
-
-            for (int i = valorInicial; i <= valorFinal ; i++) {
-
-
-                while (pausado) {
-                    Thread.sleep(1000);
-                    updateMessage("Tarea PAUSADA");
-                }
-
-                if (isCancelled()) {
-                    break;
-                }
-
-                porcentaje = i * 100 / valorFinal;
-
-                if (porcentaje >= 50 && porcentaje < 75) {
-                    updateMessage(i + " de " + valorFinal + "\n" + porcentaje + " %" + "\n" + "Vas por el 50 %" );
-                } else if (porcentaje >= 75) {
-                    updateMessage(i + " de " + valorFinal + "\n" + porcentaje + " %" + "\n" + "Vas por el 75 %" );
-                } else {
-                    updateMessage(i + " de " + valorFinal + "\n" + porcentaje + " %" );
-                }
-
-                Thread.sleep(1000);
-
-                updateProgress(i,valorFinal);
-
-
-            }
-
-            updateMessage("Finalizado");
-            updateProgress(1,1);
+            cuentaNormal();
 
         } else {
 
-            int porcentaje;
-            double progreso = valorFinal;
-
-            for (int i = valorInicial; i >= valorFinal ; i--) {
-
-
-                while (pausado) {
-                    Thread.sleep(1000);
-                    updateMessage("Tarea PAUSADA");
-                }
-
-                if (isCancelled()) {
-                    break;
-                }
-
-                porcentaje = (int) (progreso * 100 / valorInicial);
-
-                if (porcentaje >= 50 && porcentaje < 75) {
-                    updateMessage("Cuenta atrás -> " + i + "\n" + porcentaje + " %" + "\n" + "Vas por el 50 %" );
-                } else if (porcentaje >= 75) {
-                    updateMessage("Cuenta atrás -> " + i + "\n" + porcentaje + " %" + "\n" + "Vas por el 75 %" );
-                } else {
-                    updateMessage("Cuenta atrás -> " + i + "\n" + porcentaje + " %" );
-                }
-
-                Thread.sleep(1000);
-
-                progreso++;
-
-                updateProgress(progreso,valorInicial);
-
-
-            }
-
-            updateMessage("Finalizado");
-            updateProgress(1,1);
+            cuentaAtras();
 
         }
 
         return null;
     }
 
+    public void cuentaAtras() {
+
+        int porcentaje;
+        double progreso = valorFinal;
+
+        for (int i = valorInicial; i >= valorFinal ; i--) {
+
+
+            while (pausado) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                updateMessage("Tarea PAUSADA");
+            }
+
+            if (isCancelled()) {
+                break;
+            }
+
+            porcentaje = (int) (progreso * 100 / valorInicial);
+
+            if (porcentaje >= 50 && porcentaje < 75) {
+                updateMessage("Cuenta atrás -> " + i + "\n" + porcentaje + " %" + "\n" + "Vas por el 50 %" );
+            } else if (porcentaje >= 75) {
+                updateMessage("Cuenta atrás -> " + i + "\n" + porcentaje + " %" + "\n" + "Vas por el 75 %" );
+            } else {
+                updateMessage("Cuenta atrás -> " + i + "\n" + porcentaje + " %" );
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            progreso++;
+
+            updateProgress(progreso,valorInicial);
+
+
+        }
+
+        updateMessage("Finalizado");
+        updateProgress(1,1);
+
+    }
+
+    public void cuentaNormal() {
+
+        int porcentaje;
+
+        for (int i = valorInicial; i <= valorFinal ; i++) {
+
+
+            while (pausado) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                updateMessage("Tarea PAUSADA");
+            }
+
+            if (isCancelled()) {
+                break;
+            }
+
+            porcentaje = i * 100 / valorFinal;
+
+            if (porcentaje >= 50 && porcentaje < 75) {
+                updateMessage(i + " de " + valorFinal + "\n" + porcentaje + " %" + "\n" + "Vas por el 50 %" );
+            } else if (porcentaje >= 75) {
+                updateMessage(i + " de " + valorFinal + "\n" + porcentaje + " %" + "\n" + "Vas por el 75 %" );
+            } else {
+                updateMessage(i + " de " + valorFinal + "\n" + porcentaje + " %" );
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            updateProgress(i,valorFinal);
+
+
+        }
+
+        updateMessage("Finalizado");
+        updateProgress(1,1);
+
+    }
 
 }
